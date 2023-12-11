962356473281edbd4834202d382694e1418fe6a3
DigitalRobot <digitalrobot@outlook.de>
2023-12-11 22:03:13 +0100

    feat(home): defaultApplications xdg-mime

--- home-manager/david/home.nix
+++ home-manager/david/home.nix
@@ -30,50 +30,6 @@
   xdg = {
     enable = true;
     cacheHome = config.home.homeDirectory + "/.local/cache";
-    mimeApps = {
-      #enable = true;
-      #defaultApplications = {
-      #  #$ xdg-mime query filetype <https://wiki.archlinux.org/title/Xdg-utils#xdg-mime>
-      #  #$ find / -name "*zathura*.desktop" 2>/dev/null | fzf
-      #  "application/pdf" = "org.gnome.Evince.desktop";
-      #  "application/sql" = "micro.desktop";
-      #  "application/x-shellscript" = "micro.desktop";
-      #  "audio/mp2" = "mpv.desktop";
-      #  "audio/mp4" = "mpv.desktop";
-      #  "audio/mpeg" = "mpv.desktop";
-      #  "audio/ogg" = "mpv.desktop";
-      #  "inode/directory" = "pcmanfm.desktop";
-      #  "image/avif" = "imv-dir.desktop";
-      #  "image/bmp" = "imv-dir.desktop";
-      #  "image/gif" = "imv-dir.desktop";
-      #  "image/heic" = "imv-dir.desktop";
-      #  "image/heif" = "imv-dir.desktop";
-      #  "image/jp2" = "imv-dir.desktop";
-      #  "image/jpeg" = "imv-dir.desktop";
-      #  "image/jpg" = "imv-dir.desktop";
-      #  "image/jxl" = "imv-dir.desktop";
-      #  "image/png" = "imv-dir.desktop";
-      #  "image/svg+xml" = "org.gnome.eog.desktop";
-      #  "image/tiff" = "imv-dir.desktop";
-      #  "image/webp" = "imv-dir.desktop";
-      #  "image/x-bmp" = "imv-dir.desktop";
-      #  "image/x-portable-anymap" = "imv-dir.desktop";
-      #  "image/x-portable-bitmap" = "imv-dir.desktop";
-      #  "image/x-portable-graymap" = "imv-dir.desktop";
-      #  "image/x-tga" = "imv-dir.desktop";
-      #  "image/x-xpixmap" = "imv-dir.desktop";
-      #  "text/csv" = "micro.desktop";
-      #  "text/html" = "micro.desktop";
-      #  "text/plain" = "org.gnome.gedit.desktop";
-      #  "text/x-scss" = "micro.desktop";
-      #  "video/mp4" = "mpv.desktop";
-      #  "video/mpeg" = "mpv.desktop";
-      #  "video/ogg" = "mpv.desktop";
-      #  "video/quicktime" = "mpv.desktop";
-      #  "video/vivo" = "mpv.desktop";
-      #  "video/webm" = "mpv.desktop";
-      #};
-    };
     userDirs = {
       enable = true;
       createDirectories = true;
