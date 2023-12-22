a96073d3fbe84de630c48f867b89aa0cb444bf6c
DigitalRobot <digitalrobot@outlook.de>
2023-12-22 11:56:48 +0100

    fix: gammastep

--- home-manager/david/services/gammastep.nix
+++ home-manager/david/services/gammastep.nix
@@ -1,13 +1,26 @@
 {
   services.gammastep = {
     enable = true;
-    provider = "geoclue2";
+
+    provider = "manual";
+    latitude = 48.00;
+    longitude = 12.65;
+
     temperature = {
-      day = 6000;
+      day = 6500; # neutral: 6500K
       night = 4600;
     };
     settings = {
-      general.adjustment-method = "wayland";
+      general = {
+        adjustment-method = "wayland";
+
+        #fade = "1"; # gradually apply the new screen temperature/brightness over a couple of seconds.
+        # it is a fake brightness adjustment obtained by manipulating the gamma ramps,
+        # which means that it does not reduce the backlight　of the screen.
+        # Preferably only use it if your normal backlight adjustment is too coarse-grained.
+        #brightness-day = "1.0";
+        #brightness-night = "0.8";
+      };
     };
   };
 }
--- home-manager/david/services/redshift.nix
+++
@@ -1,9 +0,0 @@
-{
-  services.redshift = {
-    enable = true;
-    latitude = "48.000";
-    longitude = "12.650";
-    temperature.night = 3800;
-    temperature.day = 6500;
-  };
-}
