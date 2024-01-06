0bea73e5f4bc9084902f9a379ca197cff1f74217
DigitalRobot <digitalrobot@outlook.de>
2024-01-06 14:20:46 +0100

    kitty: adjust special desktop & keybindings

--- home-manager/david/programs/kitty.nix
+++ home-manager/david/programs/kitty.nix
@@ -4,21 +4,51 @@
   # https://github.com/kovidgoyal/kitty
   programs.kitty = {
     enable = true;
-    # config <https://sw.kovidgoyal.net/kitty/conf/>
-    keybindings = {
-      "kitty_mod+up" = "scroll_to_prompt -1";
-      "kitty_mod+down" = "scroll_to_prompt 1";
-    };
+
     settings = {
-      #background_opacity = lib.mkDefault "0.85";
-      confirm_os_window_close = 0;
-      copy_on_select = "clipboard";
-      enable_audio_bell = false;
-      hide_window_decorations = true;
-      kitty_mod = "ctrl+shift";
-      placement_strategy = "center";
+      # https://sw.kovidgoyal.net/kitty/conf/
+
+      # Scrollback
       scrollback_lines = 10000;
+
+      # Mouse
+      copy_on_select = "clipboard";
+
+      # Terminal Bell
+      enable_audio_bell = false;
+      visual_bell_duration = "1.0";
+
+      # window layout
+      hide_window_decorations = true;
+      window_border_width = "5px";
+      active_border_color = "none";
+      inactive_border_color = "#${config.lib.stylix.colors.base01}";
+      inactive_text_alpha = "0.9";
+      #draw_minimal_borders = false;
+      #window_margin_width = 1;
+      window_padding_width = 5;
+      confirm_os_window_close = 0;
+
+      # Color scheme
+      #background_opacity = lib.mkDefault "0.85";
+
+      # Advanced
       update_check_interval = 0;
+
+      # Keyboard Shortcuts
+      kitty_mod = "ctrl+shift";
+    };
+
+    keybindings = {
+      # https://sw.kovidgoyal.net/kitty/conf/#keyboard-shortcuts
+
+      # Scrolling
+      "kitty_mod+[" = "scroll_to_prompt -1";
+      "kitty_mod+]" = "scroll_to_prompt 1";
+
+      # Window management
+      "kitty_mod+up" = "next_window";
+      "kitty_mod+down" = "previous_window";
     };
   };
 
@@ -36,8 +66,8 @@
       layout fat:mirrored=true;bias=60;full_size=1
       launch ${pkgs.btop}/bin/btop
       launch ${pkgs.asciiquarium}/bin/asciiquarium
-      launch
-      launch ${pkgs.pipes}/bin/pipes.sh -f20 -s14 -t1
+      launch ${pkgs.peaclock}/bin/peaclock
+      launch ${pkgs.pipes}/bin/pipes.sh -f20 -s12 -t1
 
       new_tab
       launch --title "kitty" ${config.programs.kitty.package}/bin/kitten @
