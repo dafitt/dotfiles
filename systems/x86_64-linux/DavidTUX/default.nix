0b0d05918d776a99057313420bcdb4fdde34809d
dafitt <dafitt@posteo.me>
2024-03-15 22:49:55 +0100

    DavidTUX: fix

--- systems/x86_64-linux/DavidTUX/default.nix
+++ systems/x86_64-linux/DavidTUX/default.nix
@@ -7,10 +7,7 @@
   custom = {
     battery.enable = true;
     bootloader.grub.enable = true;
-    desktops.gnome.enable = true;
     desktops.hyprland.enable = true;
-    development.sshAgent.enable = true;
-    displayManager.gdm.enable = true;
     flatpak.enable = true;
     networking.connman.enable = true;
   };
