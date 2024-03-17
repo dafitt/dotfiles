1daca082903652ea2c9e5c308f991ce90d9beb4a
dafitt <dafitt@posteo.me>
2024-03-17 18:04:50 +0100

    feat: direnv in nixos

--- modules/home/development/direnv/default.nix
+++ modules/home/development/direnv/default.nix
@@ -1,18 +1,18 @@
-{ options, config, lib, pkgs, ... }:
+{ options, config, lib, pkgs, osConfig ? { }, ... }:
 
 with lib;
 with lib.custom;
 let
   cfg = config.custom.development.direnv;
+  osCfg = osConfig.custom.development.direnv or null;
 in
 {
   options.custom.development.direnv = with types; {
-    enable = mkBoolOpt config.custom.development.enableSuite "Enable direnv";
+    enable = mkBoolOpt (!osCfg.enable or config.custom.development.enableSuite) "Enable direnv";
   };
 
   config = mkIf cfg.enable {
     programs.direnv.enable = true;
-
     home.sessionVariables.DIRENV_LOG_FORMAT = ""; # silents direnv
   };
 }
---
+++ modules/nixos/development/direnv/default.nix
@@ -0,0 +1,17 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.custom;
+let
+  cfg = config.custom.development.direnv;
+in
+{
+  options.custom.development.direnv = with types; {
+    enable = mkBoolOpt config.custom.development.enableSuite "Enable direnv";
+  };
+
+  config = mkIf cfg.enable {
+    programs.direnv.enable = true;
+    programs.direnv.silent = true;
+  };
+}
