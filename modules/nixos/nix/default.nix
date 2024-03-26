1e34a40345e3ed18c72e70f9c7491ceaab3f9536
dafitt <dafitt@posteo.me>
2024-03-26 15:08:36 +0100

    refactor: dont allow to disable nix-configuration

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -2,57 +2,48 @@
 
 with lib;
 with lib.custom;
-let
-  cfg = config.custom.nix;
-in
 {
-  options.custom.nix = with types; {
-    enable = mkBoolOpt true "Whether or not to manage nix configuration.";
-  };
+  environment.systemPackages = with pkgs; with inputs; [
+    snowfall-flake.packages.${system}.default
+  ];
 
-  config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; with inputs; [
-      snowfall-flake.packages.${system}.default
-    ];
+  programs.git.enable = true;
 
-    programs.git.enable = true;
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      http-connections = 50;
+      warn-dirty = false;
+      log-lines = 50;
+      sandbox = "relaxed";
+      auto-optimise-store = true;
+      trusted-users = [ "root" ];
+      allowed-users = [ "@wheel" ];
+    } // (lib.optionalAttrs config.custom.development.direnv.enable {
+      keep-outputs = true;
+      keep-derivations = true;
+    });
 
-    nix = {
-      settings = {
-        experimental-features = "nix-command flakes";
-        http-connections = 50;
-        warn-dirty = false;
-        log-lines = 50;
-        sandbox = "relaxed";
-        auto-optimise-store = true;
-        trusted-users = [ "root" ];
-        allowed-users = [ "@wheel" ];
-      } // (lib.optionalAttrs config.custom.development.direnv.enable {
-        keep-outputs = true;
-        keep-derivations = true;
-      });
-
-      gc = {
-        automatic = true;
-        dates = "weekly";
-        options = "--delete-older-than 7d";
-      };
-
-      registry."nixpkgs".flake = inputs.nixpkgs; # Make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
-      registry."unstable".flake = inputs.unstable;
-
-      # disable nix-channel, we use flakes instead.
-      channel.enable = false;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
     };
 
-    # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
-    environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
-    nix.nixPath = [ "/etc/nix/inputs" ];
+    registry."nixpkgs".flake = inputs.nixpkgs; # Make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
+    registry."unstable".flake = inputs.unstable;
 
-    # repair command-not-found database
-    programs.command-not-found.dbPath = inputs.programsdb.packages.${pkgs.system}.programs-sqlite;
-
-    # Multitheaded and faster building (make)
-    environment.variables.MAKEFLAGS = "-j$(expr $(nproc) \+ 1)";
+    # disable nix-channel, we use flakes instead.
+    channel.enable = false;
   };
+
+  # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
+  environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
+  nix.nixPath = [ "/etc/nix/inputs" ];
+
+  # repair command-not-found database
+  programs.command-not-found.dbPath = inputs.programsdb.packages.${pkgs.system}.programs-sqlite;
+
+  # Multitheaded and faster building (make)
+  environment.variables.MAKEFLAGS = "-j$(expr $(nproc) \+ 1)";
 }
