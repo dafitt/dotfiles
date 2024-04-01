6a2f0061c0353d1d53e30af3e41f01be5b85bb39
dafitt <dafitt@posteo.me>
2024-04-01 12:40:57 +0200

    nixos: tweaked nix
    tried to fix `nix shell nixpkgs#yazi' endlessly building

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -11,15 +11,16 @@ with lib.custom;
 
   nix = {
     settings = {
-      experimental-features = [ "nix-command" "flakes" ];
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
       http-connections = 50;
       warn-dirty = false;
       log-lines = 50;
-      sandbox = "relaxed";
       auto-optimise-store = true;
+      trusted-substituters = config.nix.settings.substituters;
       trusted-users = [ "root" ];
-      allowed-users = [ "@wheel" ];
-    } // (lib.optionalAttrs config.custom.development.direnv.enable {
+      allowed-users = [ "@wheel" config.custom.users.main.username ];
+    }
+    // (lib.optionalAttrs config.custom.development.direnv.enable {
       keep-outputs = true;
       keep-derivations = true;
     });
@@ -30,16 +31,26 @@ with lib.custom;
       options = "--delete-older-than 7d";
     };
 
-    registry."nixpkgs".flake = inputs.nixpkgs; # Make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
+    # TODO: 24.05 remove nixpkgs, see: https://github.com/NixOS/nixpkgs/pull/254405
+    # Make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
+    #registry."nixpkgs".flake = inputs.nixpkgs;
+    registry."nixpkgs".to = {
+      type = "path";
+      path = pkgs.path;
+      narHash = builtins.readFile (pkgs.runCommandLocal "get-nixpkgs-hash"
+        { nativeBuildInputs = [ pkgs.nix ]; }
+        "nix-hash --type sha256 --sri ${pkgs.path} > $out");
+    };
+    nixPath = [ "nixpkgs=flake:nixpkgs" ];
     registry."unstable".flake = inputs.unstable;
 
     # disable nix-channel, we use flakes instead.
     channel.enable = false;
   };
 
+  # but NIX_PATH is still used by many useful tools, so we set it to the same value as the one used by this flake.
   # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
   environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
-  nix.nixPath = [ "/etc/nix/inputs" ];
 
   # repair command-not-found database
   programs.command-not-found.dbPath = inputs.programsdb.packages.${pkgs.system}.programs-sqlite;
