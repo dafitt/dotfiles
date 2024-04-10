fc7b6d1a39b980370ac4a9ca6f0e0d463b1aafa3
dafitt <dafitt@posteo.me>
2024-04-10 09:12:43 +0200

    DavidVPN: dont autostart

--- systems/x86_64-linux/DavidLEGION/DavidVPN.nix
+++ systems/x86_64-linux/DavidLEGION/DavidVPN.nix
@@ -1,9 +1,13 @@
-{ pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+
+with lib;
+{
   #$ sudo systemctl status wg-quick-DavidVPN
   #$ sudo systemctl stop wg-quick-DavidVPN
   #$ sudo systemctl start wg-quick-DavidVPN
   services.connman.networkInterfaceBlacklist = [ "DavidVPN" ];
   networking.wg-quick.interfaces."DavidVPN" = {
+    autostart = false;
     address = [ "fc07::2/64" "10.0.0.2/24" ];
     dns = [ "fd02:f69b:7377:a500::8" "192.168.19.132" ];
 
@@ -25,7 +29,6 @@
 
   systemd.services."wg-quick-DavidVPN-restart" = {
     description = "restart DavidVPN"; # because of endpoint's dyndns
-    wantedBy = [ "multi-user.target" ];
     wants = [ "network-online.target" ];
     after = [ "nss-lookup.target" ];
     serviceConfig = {
@@ -33,14 +36,15 @@
       ExecStart = "${pkgs.systemd}/bin/systemctl restart wg-quick-DavidVPN";
       Restart = "on-failure";
     };
+    wantedBy = optional config.networking.wg-quick.interfaces."DavidVPN".autostart "multi-user.target";
   };
   systemd.timers."wg-quick-DavidVPN-restart" = {
-    wantedBy = [ "timers.target" ];
     wants = [ "network-online.target" ];
     after = [ "nss-lookup.target" ];
     timerConfig = {
       OnCalendar = "daily";
       Persistent = true;
     };
+    wantedBy = optional config.networking.wg-quick.interfaces."DavidVPN".autostart "timers.target";
   };
 }
