650060000e94a304aa2c6cdbfd137e6952cf0da5
dafitt <dafitt@posteo.me>
2024-04-11 12:41:43 +0200

    flake: reodered and updated inputs

--- flake.nix
+++ flake.nix
@@ -1,36 +1,31 @@
 {
-  description = "Dafitt's desktop flake";
-
   #$ flake update [inputs]
   #$ nix flake update [--commit-lock-file]
   #$ nix flake lock --update-input [input]
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11"; # https://github.com/NixOS/nixpkgs
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nur.url = "github:nix-community/NUR"; # https://github.com/nix-community/NUR
 
-    home-manager = { url = "github:nix-community/home-manager/release-23.11"; inputs.nixpkgs.follows = "nixpkgs"; };
+    home-manager = { url = "github:nix-community/home-manager/release-23.11"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/nix-community/home-manager
+    nixos-generators = { url = "github:nix-community/nixos-generators"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/nix-community/nixos-generators
 
-    nixos-generators = { url = "github:nix-community/nixos-generators"; inputs.nixpkgs.follows = "nixpkgs"; };
+    snowfall-lib = { url = "github:snowfallorg/lib/dev"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/snowfallorg/lib
+    snowfall-flake = { url = "github:snowfallorg/flake"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/snowfallorg/flake
 
-    snowfall-lib = { url = "github:snowfallorg/lib/dev"; inputs.nixpkgs.follows = "nixpkgs"; };
-    snowfall-flake = { url = "github:snowfallorg/flake"; inputs.nixpkgs.follows = "nixpkgs"; };
+    stylix.url = "github:danth/stylix/release-23.11"; # https://github.com/danth/stylix
 
+    hyprland.url = "github:hyprwm/hyprland/?ref=v0.38.1"; # https://github.com/hyprwm/hyprland
+    hyprland-plugins = { url = "github:hyprwm/hyprland-plugins"; inputs.hyprland.follows = "hyprland"; }; # https://github.com/hyprwm/hyprland-plugins
+    hypridle = { url = "github:hyprwm/hypridle"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/hyprwm/hypridle
+    hyprlock = { url = "github:hyprwm/hyprlock"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/hyprwm/hyprlock
+    hyprpaper = { url = "github:hyprwm/hyprpaper"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/hyprwm/hyprpaper
+    hyprwm-contrib.url = "github:hyprwm/contrib"; # https://github.com/hyprwm/contrib
+    hyprkeys = { url = "github:hyprland-community/hyprkeys"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/hyprland-community/hyprkeys
 
-    stylix.url = "github:danth/stylix/release-23.11";
+    programsdb = { url = "github:wamserma/flake-programs-sqlite"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/wamserma/flake-programs-sqlite
 
-    hyprland.url = "github:hyprwm/hyprland";
-    hyprwm-contrib.url = "github:hyprwm/contrib";
-    hyprland-plugins = { url = "github:hyprwm/hyprland-plugins"; inputs.hyprland.follows = "hyprland"; };
-    hypridle = { url = "github:hyprwm/hypridle"; inputs.nixpkgs.follows = "nixpkgs"; };
-    hyprkeys = { url = "github:hyprland-community/hyprkeys"; inputs.nixpkgs.follows = "nixpkgs"; };
-    hyprlock = { url = "github:hyprwm/hyprlock"; inputs.nixpkgs.follows = "nixpkgs"; };
-    hyprpaper = { url = "github:hyprwm/hyprpaper"; inputs.nixpkgs.follows = "nixpkgs"; };
-
-    programsdb = { url = "github:wamserma/flake-programs-sqlite"; inputs.nixpkgs.follows = "nixpkgs"; };
-
-    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.3.0";
-
-    nur.url = "github:nix-community/NUR"; # Nix User Repository
+    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.3.0"; # https://github.com/gmodena/nix-flatpak
   };
 
   # [Snowfall framework](https://snowfall.org/guides/lib/quickstart/)
