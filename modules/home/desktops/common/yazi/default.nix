eb95d2eb55bc8d816ad2b87067a048554f19b78f
dafitt <dafitt@posteo.me>
2024-04-15 08:23:08 +0200

    feat: added yazi file manager

---
+++ modules/home/desktops/common/yazi/default.nix
@@ -0,0 +1,40 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.dafitt;
+let
+  cfg = config.dafitt.desktops.common.yazi;
+in
+{
+  options.dafitt.desktops.common.yazi = with types; {
+    enable = mkBoolOpt config.dafitt.desktops.common.enable "Enable the yazi terminal file manager";
+  };
+
+  config = mkIf cfg.enable {
+    # [Documentation](https://yazi-rs.github.io/docs/quick-start/)
+    programs.yazi = {
+      enable = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+      enableNushellIntegration = true;
+      enableZshIntegration = true;
+    };
+
+    # https://github.com/sxyazi/yazi/blob/main/assets/yazi.desktop
+    xdg.desktopEntries.yazi = {
+      name = "Yazi";
+      icon = ./logo.png;
+      genericName = "File Manager";
+      comment = "Blazing fast terminal file manager written in Rust, based on async I/O";
+      exec = ''${config.programs.yazi.package}/bin/yazi %u'';
+      terminal = true;
+      mimeType = [ "inode/directory" ];
+      categories = [ "Utility" "Core" "System" "FileTools" "FileManager" "ConsoleOnly" ];
+
+      settings = {
+        TryExec = ''${config.programs.yazi.package}/bin/yazi'';
+        Keywords = "File;Manager;Explorer;Browser;Launcher";
+      };
+    };
+  };
+}
