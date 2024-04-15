f932b021f0f5be760bf83f32cc3a8726db140f47
dafitt <dafitt@posteo.me>
2024-04-15 08:44:40 +0200

    micro: added desktopEntry

--- modules/home/desktops/common/micro/default.nix
+++ modules/home/desktops/common/micro/default.nix
@@ -27,5 +27,24 @@ in
         eofnewline = false;
       };
     };
+
+    # https://github.com/zyedidia/micro/blob/master/assets/packaging/micro.desktop
+    xdg.desktopEntries.micro = {
+      name = "Micro";
+      icon = ./micro-logo-mark.svg;
+      genericName = "Text Editor";
+      comment = "Edit text files in a terminal";
+
+      type = "Application";
+      exec = "${pkgs.micro}/bin/micro %F";
+      terminal = true;
+      startupNotify = false;
+      mimeType = [ "text/plain" "text/x-chdr" "text/x-csrc" "text/x-c++hdr" "text/x-c++src" "text/x-java" "text/x-dsrc" "text/x-pascal" "text/x-perl" "text/x-python" "application/x-php" "application/x-httpd-php3" "application/x-httpd-php4" "application/x-httpd-php5" "application/xml" "text/html" "text/css" "text/x-sql" "text/x-diff" ];
+      categories = [ "Utility" "TextEditor" "Development" ];
+
+      settings = {
+        Keywords = "text;editor;syntax;terminal;";
+      };
+    };
   };
 }
