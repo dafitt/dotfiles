8de5d885df77742e7ddcbfe1928445240b905991
dafitt <dafitt@posteo.me>
2024-04-15 23:39:10 +0200

    templates: refactored to the defaults

--- templates/system/default.nix
+++ templates/system/default.nix
@@ -9,31 +9,32 @@
   imports = [ ./hardware-configuration.nix ];
 
   dafitt = {
-    appimage.enable = true;
+    # These are the defaults
+    appimage.enable = false;
     audio.enable = true;
-    battery.enable = true;
-    bootloader.enable = null; # enum [ "grub" "systemd-boot" ]
-    desktops.gnome.enable = true;
-    desktops.hyprland.enable = true;
-    development.direnv.enable = true;
-    development.enableSuite = true;
-    development.sshAgent.enable = true;
-    development.wireshark.enable = true;
-    displayManager.enable = null; # enum [ "gdm" "greetd" ]
-    flatpak.enable = true;
+    battery.enable = false;
+    bootloader.enable = "systemd-boot"; # nullOr (enum [ "grub" "systemd-boot" ])
+    desktops.gnome.enable = false;
+    desktops.hyprland.enable = false;
+    development.direnv.enable = false;
+    development.enableSuite = false;
+    development.sshAgent.enable = false;
+    development.wireshark.enable = false;
+    displayManager.enable = "greetd"; # nullOr (enum [ "gdm" "greetd" ])
+    flatpak.enable = false;
     fonts.enable = true;
     fstrim.enable = true;
-    fwupd.enable = true;
-    gaming.enableSuite = true;
-    gaming.steam.enable = true;
+    fwupd.enable = false;
+    gaming.enableSuite = false;
+    gaming.steam.enable = false;
     locale.enable = true;
-    networking.enable = null; # enum [ "connman" "networkmanager" ]
-    printing.enable = true;
-    security.doas.enable = true;
+    networking.enable = "networkmanager"; # nullOr (enum [ "connman" "networkmanager" ])
+    printing.enable = false;
+    security.doas.enable = false;
     shells.fish.enable = true;
-    syncthing.enable = true;
+    syncthing.openFirewall = false;
     time.enable = true;
-    virtualization.virt-manager.enable = true;
+    virtualization.virt-manager.enable = false;
   };
 
   system.stateVersion = "23.11"; # move this line to hardware-configuration.nix
