170eaadf08e91dc9222e15acfdd9169ad2b92e52
dafitt <dafitt@posteo.me>
2024-04-17 17:35:44 +0200

    steam: added plugins

--- modules/home/gaming/steam/default.nix
+++ modules/home/gaming/steam/default.nix
@@ -12,11 +12,28 @@ in
   };
 
   config = mkIf cfg.enable {
-    services.flatpak.packages = [
-      "com.valvesoftware.Steam"
-      "org.freedesktop.Platform.VulkanLayer.gamescope"
-      "com.valvesoftware.Steam.Utility.steamtinkerlaunch"
-    ];
+    # [Using Steam in a Flatpak](https://steamcommunity.com/sharedfiles/filedetails/?id=2615011323)
+    # [Steam-NixOS](https://github.com/Jovian-Experiments/Jovian-NixOS)
+
+    services.flatpak = {
+      packages = [
+        "com.valvesoftware.Steam"
+        "com.valvesoftware.SteamLink"
+        "com.valvesoftware.Steam.CompatibilityTool.Boxtron"
+        "com.valvesoftware.Steam.CompatibilityTool.Proton-GE"
+        "com.valvesoftware.Steam.Utility.protontricks"
+        "com.valvesoftware.Steam.Utility.steamtinkerlaunch"
+        "org.freedesktop.Platform.VulkanLayer.gamescope//23.08"
+        "org.freedesktop.Platform.VulkanLayer.MangoHud//23.08"
+        "org.freedesktop.Platform.VulkanLayer.vkBasalt//23.08"
+      ];
+      overrides = {
+        "com.valvesoftware.Steam".Environment = {
+          ENABLE_VKBASALT = "1";
+          MANGOHUD = "1";
+        };
+      };
+    };
 
     wayland.windowManager.hyprland.settings.windowrulev2 = [
       "bordercolor rgb(1887d8), class:steam"
