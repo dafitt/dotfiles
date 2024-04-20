2eb3e251bef8e09d027fcad0e29a273431d0c469
dafitt <dafitt@posteo.me>
2024-04-20 23:38:42 +0200

    shell: reduced to only the essential packages

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -1,18 +1,12 @@
 { pkgs, inputs, ... }:
 
+#$ flake dev
+#$ nix develop
 pkgs.mkShell {
-  #$ flake dev
-  #$ nix develop
-
   nativeBuildInputs = with pkgs; with inputs; [
-    snowfall-flake.packages.${system}.default
+    git
+    nix
     home-manager.packages.${system}.default
-    nix-prefetch-git
-
-    # Formatting
-    treefmt
-    nixpkgs-fmt
-    python310Packages.mdformat
-    shfmt
+    snowfall-flake.packages.${system}.default
   ];
 }
