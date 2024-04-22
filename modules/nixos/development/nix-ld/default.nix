986411e6d2e6e63c4934a5688e5ad9588a8eef84
dafitt <dafitt@posteo.me>
2024-04-22 19:55:04 +0200

    nixos: development: added nix-ld
    to allow running unpatched dynamic binaries on NixOS

---
+++ modules/nixos/development/nix-ld/default.nix
@@ -0,0 +1,20 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.dafitt;
+let
+  cfg = config.dafitt.development.nix-ld;
+in
+{
+  options.dafitt.development.nix-ld = with types; {
+    enable = mkBoolOpt config.dafitt.development.enableSuite "Allow running unpatched dynamic binaries on NixOS";
+  };
+
+  config = mkIf cfg.enable {
+    programs.nix-ld.enable = true;
+    programs.nix-ld.libraries = with pkgs; [
+      # Add any missing dynamic libraries for unpackaged
+      # programs here, NOT in environment.systemPackages
+    ];
+  };
+}
