75c7c784f1e6046a3946d71bd8da1236b7e1666e
dafitt <dafitt@posteo.me>
2024-05-13 14:19:40 +0200

    home: hyprland: used hyprland input with cachix

--- flake.nix
+++ flake.nix
@@ -14,12 +14,11 @@
 
     stylix.url = "github:danth/stylix"; # https://github.com/danth/stylix
 
-    #NOTE We use Hyprland from Nixpkgs, but must specify the version for plugins to follow. Update the version if Hyprland updates in Nixpkgs.
     hyprland = { url = "github:hyprwm/Hyprland/tags/v0.39.1"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/hyprwm/Hyprland
     hyprland-plugins = { url = "github:hyprwm/hyprland-plugins"; inputs.hyprland.follows = "hyprland"; }; # https://github.com/hyprwm/hyprland-plugins
-    hypr-darkwindow = { url = "github:micha4w/Hypr-DarkWindow/tags/v0.36.0"; inputs.hyprland.follows = "hyprland"; }; # https://github.com/micha4w/Hypr-DarkWindow
+    hypr-darkwindow = { url = "github:micha4w/Hypr-DarkWindow/tags/v0.39.0"; inputs.hyprland.follows = "hyprland"; }; # https://github.com/micha4w/Hypr-DarkWindow
     hyprspace = { url = "github:KZDKM/Hyprspace"; inputs.hyprland.follows = "hyprland"; }; # https://github.com/KZDKM/Hyprspace
-    hyprsplit = { url = "github:shezdy/hyprsplit"; inputs.hyprland.follows = "hyprland"; }; # https://github.com/shezdy/hyprsplit
+    hyprsplit = { url = "github:shezdy/hyprsplit/tags/v0.39.1"; inputs.hyprland.follows = "hyprland"; }; # https://github.com/shezdy/hyprsplit
 
     hypridle = { url = "github:hyprwm/hypridle"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/hyprwm/hypridle
     hyprlock = { url = "github:hyprwm/hyprlock"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/hyprwm/hyprlock
@@ -29,6 +28,17 @@
 
     nix-flatpak.url = "github:gmodena/nix-flatpak/tags/v0.4.1"; # https://github.com/gmodena/nix-flatpak/tags
   };
+
+  #NOTE uncomment on your first install for faster build time
+  #nixConfig = {
+  #  extra-substituters = [
+  #    "https://hyprland.cachix.org"
+  #  ];
+  #  extra-trusted-public-keys = [
+  #    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+  #  ];
+  #};
+
 
   # [Snowfall framework](https://snowfall.org/guides/lib/quickstart/)
   #$ nix flake check --keep-going
@@ -56,10 +66,12 @@
 
     systems.modules.nixos = with inputs; [
       stylix.nixosModules.stylix
+      hyprland.nixosModules.default
     ];
 
     homes.modules = with inputs; [
       stylix.homeManagerModules.stylix
+      hyprland.homeManagerModules.default
       nix-flatpak.homeManagerModules.nix-flatpak
     ];
 
--- modules/nixos/desktops/hyprland/default.nix
+++ modules/nixos/desktops/hyprland/default.nix
@@ -13,6 +13,11 @@ in
   config = mkIf cfg.enable {
     programs.hyprland.enable = true;
 
+    nix.settings = {
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    };
+
     # [Must Have's](https://wiki.hyprland.org/Useful-Utilities/Must-have/)
     programs.light.enable = true; # Monitor backlight control
     services = {
