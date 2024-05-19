67ff178fd56c1a0b04e636b78bc36d1ac29bdb99
dafitt <dafitt@posteo.me>
2024-05-19 21:37:22 +0200

    greetd: fixed message spam over interface

--- modules/nixos/displayManager/greetd/default.nix
+++ modules/nixos/displayManager/greetd/default.nix
@@ -38,5 +38,18 @@ in
         };
       };
     };
+
+    # [Systemd error message gets printed over interface]https://github.com/apognu/tuigreet/issues/68
+    # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+    systemd.services.greetd.serviceConfig = {
+      Type = "idle";
+      StandardInput = "tty";
+      StandardOutput = "tty";
+      StandardError = "journal"; # Without this errors will spam on screen
+      # Without these bootlogs will spam on screen
+      TTYReset = true;
+      TTYVHangup = true;
+      TTYVTDisallocate = true;
+    };
   };
 }
