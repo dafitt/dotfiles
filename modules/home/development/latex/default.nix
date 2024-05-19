3c7fa163b3005adbb71822c5c061c1dc0f1dfbc7
dafitt <dafitt@posteo.me>
2024-05-19 21:28:39 +0200

    home: latex: added TeXstudio

--- modules/home/development/latex/default.nix
+++ modules/home/development/latex/default.nix
@@ -13,12 +13,10 @@ in
   config = mkIf cfg.enable {
     services.flatpak = {
       packages = [
-        { appId = "app.gummi.gummi"; origin = "flathub"; } # LaTeX Editor
+        { appId = "app.gummi.gummi"; origin = "flathub"; } # simple and fast LaTeX editor
+        { appId = "org.texstudio.TeXstudio"; origin = "flathub"; } # more feature-rich LaTeX editor
         { appId = "fyi.zoey.TeX-Match"; origin = "flathub"; } # find LaTeX symbols by sketching
-
-        #{ appId = "org.cvfosammmm.Setzer"; origin = "flathub"; } # LaTeX Editor
       ];
-
       overrides = {
         "app.gummi.gummi".Context.filesystems = [ "home" ];
       };
