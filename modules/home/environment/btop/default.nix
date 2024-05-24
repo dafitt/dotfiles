5f8260894c67b7cf7f5743286d706fb3be8ff805
dafitt <dafitt@posteo.me>
2024-05-24 12:32:51 +0200

    environment: added btop

--- modules/home/environment/btop/default.nix
+++ modules/home/environment/btop/default.nix
@@ -3,25 +3,27 @@
 with lib;
 with lib.dafitt;
 let
-  cfg = config.dafitt.desktops.hyprland.top;
+  cfg = config.dafitt.environment.btop;
 in
 {
-  options.dafitt.desktops.hyprland.top = with types; {
-    enable = mkBoolOpt config.dafitt.desktops.hyprland.enable "Enable a top for hyprland";
+  options.dafitt.environment.btop = with types; {
+    enable = mkBoolOpt config.dafitt.environment.enable "Enable a btop";
   };
 
   config = mkIf cfg.enable {
     programs.btop = {
       enable = true;
       settings = {
+        # https://github.com/aristocratos/btop#configurability
         theme_background = false;
+        presets = "cpu:0:default";
       };
     };
 
     wayland.windowManager.hyprland.settings = {
       bind = [ "SUPER_ALT, P, exec, ${config.home.sessionVariables.TERMINAL} -e ${config.programs.btop.package}/bin/btop" ];
       windowrulev2 = [
-        "float, title:^btop$, class:kitty"
+        "float, title:^btop$, class:kitty" #TODO move to a scratchapd
         "size 90% 90%, title:^btop$"
         "minsize 800 530, title:^btop$"
         "center, title:^btop$"
