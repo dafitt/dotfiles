df5857711f38931b820bceaf1df30ebe3fb64a34
dafitt <dafitt@posteo.me>
2024-06-15 11:50:14 +0200

    hyprland-plugins: activated hyprexpo

--- modules/home/desktops/hyprland/plugins/hyprexpo/default.nix
+++ modules/home/desktops/hyprland/plugins/hyprexpo/default.nix
@@ -7,7 +7,7 @@ let
 in
 {
   options.dafitt.desktops.hyprland.plugins.hyprexpo = with types; {
-    enable = mkBoolOpt false "Enable hyprexpo hyprland plugin.";
+    enable = mkBoolOpt config.dafitt.desktops.hyprland.plugins.enable "Enable hyprexpo hyprland plugin.";
   };
 
   config = mkIf cfg.enable {
@@ -15,19 +15,19 @@ in
       # https://github.com/hyprwm/hyprland-plugins/tree/main/hyprexpo
       plugins = with pkgs; [ inputs.hyprland-plugins.packages.${system}.hyprexpo ];
 
-      settings.bind = [ "SUPER, grave, hyprexpo:expo, toggle" ]; # can be: toggle, off/disable or on/enable
+      settings.bind = [ "SUPER, acute, hyprexpo:expo, toggle" ]; # can be: toggle, off/disable or on/enable
 
       extraConfig = ''
         plugin {
           hyprexpo {
             columns = 3
-            gap_size = 5
-            bg_col = rgb(111111)
+            gap_size = ${toString config.wayland.windowManager.hyprland.settings.general.gaps_in}
+            bg_col = rgb(${config.lib.stylix.colors.base06})
             workspace_method = center current # [center/first] [workspace] e.g. first 1 or center m+1
 
             enable_gesture = true # laptop touchpad, 4 fingers
             gesture_distance = 300 # how far is the "max"
-            gesture_positive = true # positive = swipe down. Negative = swipe up.
+            gesture_positive = false # positive = swipe down. Negative = swipe up.
           }
         }
       '';
