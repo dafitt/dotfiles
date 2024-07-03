54592971e436c4ebe286fee91dc6c74c9620b344
dafitt <dafitt@posteo.me>
2024-07-03 20:58:05 +0200

    pyprland: improved ExecStop command

--- modules/home/desktops/hyprland/pyprland/default.nix
+++ modules/home/desktops/hyprland/pyprland/default.nix
@@ -85,7 +85,7 @@ in
       Service = {
         Type = "simple";
         ExecStart = "${pkgs.pyprland}/bin/pypr";
-        ExecStop = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/rm /run/user/%U/hypr/*/.pyprland.sock'";
+        ExecStop = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/rm $XDG_RUNTIME_DIR/hypr/*/.pyprland.sock'";
         Restart = "on-failure";
         X-Restart-Triggers = [ "${config.xdg.configFile."hypr/pyprland.toml".source}" ];
       };
