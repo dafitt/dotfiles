f9709adbb30bdb23464c2d9bb86f18160a9251dc
dafitt <dafitt@posteo.me>
2024-08-07 13:18:30 +0200

    gnome: added extension 'just-perfection'

---
+++ modules/home/desktops/gnome/extensions/just-perfection/default.nix
@@ -0,0 +1,29 @@
+{ options, config, lib, pkgs, inputs, osConfig ? { }, ... }:
+
+with lib;
+with lib.dafitt;
+let
+  cfg = config.dafitt.desktops.gnome.extensions.just-perfection;
+in
+{
+  options.dafitt.desktops.gnome.extensions.just-perfection = with types; {
+    enable = mkBoolOpt config.dafitt.desktops.gnome.extensions.enable "Enable Gnome extension 'just-perfection'.";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs.gnomeExtensions; [ just-perfection ];
+
+    dconf.settings = {
+      "org/gnome/shell" = {
+        enabled-extensions = [ "just-perfection-desktop@just-perfection" ];
+      };
+      "org/gnome/shell/extensions/just-perfection" = {
+        #top-panel-position = 1; # bottom
+        animation = 4;
+        dash = false;
+        double-super-to-appgrid = false;
+        window-demands-attention-focus = true;
+      };
+    };
+  };
+}
