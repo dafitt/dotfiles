3e7b22bbdf34c7a5c848caf6805f86622021f85d
dafitt <dafitt@posteo.me>
2024-08-07 12:46:35 +0200

    gnome: added extension 'auto-move-windows' (disabled)

--- modules/home/desktops/gnome/extensions/default.nix
+++ modules/home/desktops/gnome/extensions/default.nix
@@ -16,29 +16,6 @@ in
     dconf.settings = {
       "org/gnome/shell" = {
         disable-user-extensions = false;
-
-        favorite-apps = [
-          "librewolf.desktop"
-          "org.wezfurlong.wezterm.desktop"
-          "org.gnome.Nautilus.desktop"
-          "org.gnome.Calendar.desktop"
-          "obsidian.desktop"
-          "transmission-gtk.desktop"
-          "caprine.desktop"
-          "teams-for-linux.desktop"
-          "discord.desktop"
-          "spotify.desktop"
-          "com.usebottles.bottles.desktop"
-          "org.gnome.Software.desktop"
-        ];
-      };
-      "org/gnome/shell/extensions/auto-move-windows" = {
-        application-list = [
-          "librewolf.desktop:1"
-          "org.gnome.Nautilus.desktop:2"
-          "md.obsidian.Obsidian.desktop:3"
-          "code.desktop:4"
-        ];
       };
     };
   };
