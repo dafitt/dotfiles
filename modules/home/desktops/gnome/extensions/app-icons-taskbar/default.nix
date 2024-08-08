fbf9e7ac41a71a653eba7c8b4adfdf8917803d0f
dafitt <dafitt@posteo.me>
2024-08-08 14:26:12 +0200

    gnome: added extension 'app-icons-taskbar'

---
+++ modules/home/desktops/gnome/extensions/app-icons-taskbar/default.nix
@@ -0,0 +1,28 @@
+{ options, config, lib, pkgs, inputs, osConfig ? { }, ... }:
+
+with lib;
+with lib.dafitt;
+let
+  cfg = config.dafitt.desktops.gnome.extensions.app-icons-taskbar;
+in
+{
+  options.dafitt.desktops.gnome.extensions.app-icons-taskbar = with types; {
+    enable = mkBoolOpt config.dafitt.desktops.gnome.extensions.enable "Enable Gnome extension 'app-icons-taskbar'.";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs.gnomeExtensions; [ app-icons-taskbar ]
+      ++ [ pkgs.imagemagick ];
+
+    dconf.settings = {
+      "org/gnome/shell/extensions/aztaskbar" = {
+        favorites = false;
+        icon-size = 24;
+        middle-click-action = "RAISE";
+        override-panel-clock-format = (config.lib.gvariant.mkTuple [ true "%F :: %A :: %R" ]);
+        position-offset = 3;
+        window-previews = false;
+      };
+    };
+  };
+}
