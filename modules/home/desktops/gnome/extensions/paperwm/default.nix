9bb6206a95ee7587bf212018f133028b10663cba
dafitt <dafitt@posteo.me>
2024-09-12 13:26:25 +0200

    paperwm: adjusted keybindings

--- modules/home/desktops/gnome/extensions/paperwm/default.nix
+++ modules/home/desktops/gnome/extensions/paperwm/default.nix
@@ -23,11 +23,19 @@ in
         show-workspace-indicator = false;
       };
       "org/gnome/shell/extensions/paperwm/keybindings" = {
-        new-window = [ "<Super>n" ];
-        toggle-maximize-width = [ "" ];
-        paper-toggle-fullscreen = [ "" ];
         barf-out = [ "<Shift><Super>o" ];
         barf-out-active = [ "<Super>o" ];
+        move-down = [ "<Shift><Super>Down" ];
+        move-left = [ "<Shift><Super>comma" "<Shift><Super>Left" ];
+        move-right = [ "<Shift><Super>period" "<Shift><Super>Right" ];
+        move-up = [ "<Shift><Super>Up" ];
+        new-window = [ "<Super>n" ];
+        paper-toggle-fullscreen = [ "" ];
+        switch-monitor-down = [ "<Control><Super>Down" ];
+        switch-monitor-left = [ "<Control><Super>Left" ];
+        switch-monitor-right = [ "<Control><Super>Right" ];
+        switch-monitor-up = [ "<Control><Super>Up" ];
+        toggle-maximize-width = [ "" ];
       };
     };
   };
