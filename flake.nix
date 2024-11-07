274afcea3fbfca80a49622d43c43af93723fb569
dafitt <dafitt@posteo.me>
2024-11-07 08:38:45 +0100

    flake: alphabetical sorting of inputs

--- flake.nix
+++ flake.nix
@@ -1,30 +1,23 @@
 {
   #$ flake update [inputs]
   #$ nix flake update [--commit-lock-file]
-  #$ nix flake lock --update-input <input>
+  #$ nix flake update <input>
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # https://github.com/NixOS/nixpkgs
-    nur.url = "github:nix-community/NUR"; # https://github.com/nix-community/NUR
-
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/nix-community/home-manager
-    nixos-generators = { url = "github:nix-community/nixos-generators"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/nix-community/nixos-generators
-    nixos-hardware.url = "github:nixos/nixos-hardware/master"; # https://github.com/NixOS/nixos-hardware
-
-    snowfall-lib = { url = "github:snowfallorg/lib/dev"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/snowfallorg/lib
-    snowfall-flake = { url = "github:snowfallorg/flake"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/snowfallorg/flake
-
-    stylix.url = "github:danth/stylix"; # https://github.com/danth/stylix
-
     hypr-darkwindow = { url = "github:micha4w/Hypr-DarkWindow"; }; # https://github.com/micha4w/Hypr-DarkWindow
     hyprspace = { url = "github:KZDKM/Hyprspace"; }; # https://github.com/KZDKM/Hyprspace
     hyprsplit = { url = "github:shezdy/hyprsplit"; }; # https://github.com/shezdy/hyprsplit
-
-    programsdb = { url = "github:wamserma/flake-programs-sqlite"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/wamserma/flake-programs-sqlite
-
     nix-flatpak.url = "github:gmodena/nix-flatpak/v0.4.1"; # https://github.com/gmodena/nix-flatpak/tags
+    nixos-generators = { url = "github:nix-community/nixos-generators"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/nix-community/nixos-generators
+    nixos-hardware.url = "github:nixos/nixos-hardware/master"; # https://github.com/NixOS/nixos-hardware
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # https://github.com/NixOS/nixpkgs
+    nur.url = "github:nix-community/NUR"; # https://github.com/nix-community/NUR
+    programsdb = { url = "github:wamserma/flake-programs-sqlite"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/wamserma/flake-programs-sqlite
+    snowfall-flake = { url = "github:snowfallorg/flake"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/snowfallorg/flake
+    snowfall-lib = { url = "github:snowfallorg/lib/dev"; inputs.nixpkgs.follows = "nixpkgs"; }; # https://github.com/snowfallorg/lib
+    stylix.url = "github:danth/stylix"; # https://github.com/danth/stylix
 
     # for development; see overlays/-git/default.nix
-    #$ nix flake lock --update-input <input>
     pyprland.url = "github:hyprland-community/pyprland"; # https://github.com/hyprland-community/pyprland
   };
 
