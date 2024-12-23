7e92d66df862fbc64460bb2efe5cd4256ce370d6
dafitt <dafitt@posteo.me>
2024-12-23 17:09:28 +0100

    users: added user 'guest'

---
+++ modules/nixos/users/guest/default.nix
@@ -0,0 +1,20 @@
+{ options, config, lib, pkgs, inputs, ... }:
+
+with lib;
+with lib.dafitt;
+let
+  cfg = config.dafitt.users.guest;
+in
+{
+  options.dafitt.users.guest = with types; {
+    enable = mkBoolOpt true "Whether to enable user 'guest'.";
+  };
+
+  config = {
+    users.users."guest" = {
+      isNormalUser = true;
+      description = "Guest Account";
+      password = "guest";
+    };
+  };
+}
