de857c25235b0ef2ec6ba241833dc89634a1f18d
dafitt <dafitt@posteo.me>
2024-12-26 18:14:24 +0100

    hyprland: added a new theme with hyprpanel

---
+++ modules/home/hyprland/themes/hyprpanel/default.nix
@@ -0,0 +1,61 @@
+{ options, config, lib, pkgs, inputs, ... }:
+
+with lib;
+with lib.dafitt;
+let
+  cfg = config.dafitt.hyprland.themes.hyprpanel;
+  hyprlandCfg = config.wayland.windowManager.hyprland;
+in
+{
+  #imports = [ inputs.hyprpanel.homeManagerModules.hyprpanel ];
+
+  options.dafitt.hyprland.themes.hyprpanel = with types; {
+    enable = mkBoolOpt true "Whether to enable the hyprpanel theme.";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.hyprpanel ];
+
+    # https://github.com/Jas-SinghFSU/HyprPanel/blob/master/nix/module.nix#L65
+    # https://hyprpanel.com/configuration/panel.html
+    programs.hyprpanel = {
+      enable = true;
+      systemd.enable = true;
+      hyprland.enable = true;
+      overwrite.enable = true;
+
+      layout."bar.layouts" = {
+        "0" = {
+          left = [ "workspaces" "windowtitle" ];
+          middle = [ "clock" "media" ];
+          right = [ "systray" "notifications" "battery" "volume" "bluetooth" "network" "dashboard" ];
+        };
+      };
+      theme = "catppuccin_mocha_vivid";
+      settings = {
+        bar.launcher.autoDetectIcon = true;
+        bar.workspaces.reverse_scroll = true;
+        bar.workspaces.show_icons = true;
+        menus.clock.time.hideSeconds = true;
+        menus.clock.time.military = true;
+        menus.clock.weather.unit = "metric";
+        menus.dashboard.directories.enabled = false;
+        menus.dashboard.stats.enable_gpu = true;
+        theme.bar.buttons.radius = "${toString hyprlandCfg.settings.decoration.rounding}px";
+        theme.bar.menus.border.radius = "${toString hyprlandCfg.settings.decoration.rounding}px";
+        theme.bar.menus.border.size = "${toString hyprlandCfg.settings.general.border_size}px";
+        theme.bar.menus.buttons.radius = "${toString hyprlandCfg.settings.decoration.rounding}px";
+        theme.bar.outer_spacing = "${toString hyprlandCfg.settings.general.gaps_out}px";
+        theme.bar.transparent = true;
+        theme.font.name = "CaskaydiaCove NF";
+        theme.font.size = "16px";
+      };
+    };
+
+    wayland.windowManager.hyprland.settings = {
+      bind = [
+        "SUPER, W, exec, hyprpanel toggleWindow bar-0"
+      ];
+    };
+  };
+}
