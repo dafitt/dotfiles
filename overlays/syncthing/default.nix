2fe7e27611fd534e8719d80a4dd6eb97af605b25
dafitt <dafitt@posteo.me>
2024-12-30 20:55:46 +0100

    syncthing: added overlay with desktopFile

---
+++ overlays/syncthing/default.nix
@@ -0,0 +1,37 @@
+{ channels, inputs, ... }:
+
+final: prev: {
+  syncthing =
+    let
+      desktopFile = prev.makeDesktopItem {
+        name = "syncthing";
+        desktopName = "Syncthing";
+        genericName = "Syncthing Web App";
+        comment = "Open Syncthing GUI in a web browser";
+        exec = "${prev.xdg-utils}/bin/xdg-open https://localhost:8384";
+        terminal = false;
+        icon = "syncthing.svg";
+        type = "Application";
+        categories = [
+          "FileTransfer"
+          "Monitor"
+          "Settings"
+          "System"
+          "Utility"
+          "X-WebApps"
+        ];
+        mimeTypes = [
+          "text/html"
+          "text/xml"
+          "application/xhtml_xml"
+        ];
+      };
+    in
+    prev.syncthing.overrideAttrs (oldAttrs: {
+      postInstall = oldAttrs.postInstall + ''
+        mkdir -p $out/share/applications
+        ln -s ${desktopFile}/share/applications/* $out/share/applications
+        #install -Dvm444 $src/assets/logo-only.svg $out/share/branding/syncthing.svg
+      '';
+    });
+}
--- templates/overlay/default.nix
+++ templates/overlay/default.nix
@@ -1,5 +1,6 @@
 { channels, inputs, ... }:
 
+# https://snowfall.org/guides/lib/overlays/
 # https://wiki.nixos.org/wiki/Overlays
 # final: nixpkgs with your overlay applied
 # prev: nixpkgs without your overlay
@@ -13,6 +14,6 @@ final: prev: {
 
   # package override
   PACKAGE_OVERRIDE_OPTION = prev.PACKAGE_OVERRIDE_OPTION.override { x11Support = false; };
-  PACKAGE_OVERRIDE_ATTRS = prev.PACKAGE_OVERRIDE_ATTRS.overrideAttrs (old: { src = prev.fetchFromGitHub { }; });
+  PACKAGE_OVERRIDE_ATTRS = prev.PACKAGE_OVERRIDE_ATTRS.overrideAttrs (oldAttrs: { src = prev.fetchFromGitHub { }; });
   PACKAGE_OVERRIDE_EXTEDND = prev.PACKAGE_OVERRIDE_EXTEDND.extend (final': prev': { });
 }
