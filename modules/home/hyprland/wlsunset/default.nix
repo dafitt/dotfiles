02ceb85c39d93e6e6c47f2aa9a9b680053a4e481
dafitt <dafitt@posteo.me>
2025-03-29 20:42:32 +0100

    wlsunset: fixed systemd target cycle

--- modules/home/hyprland/wlsunset/default.nix
+++ modules/home/hyprland/wlsunset/default.nix
@@ -13,7 +13,6 @@ in
   config = mkIf cfg.enable {
     services.wlsunset = {
       enable = true;
-      systemdTarget = "wayland-session@Hyprland.target";
 
       latitude = "48.0";
       longitude = "12.6";
