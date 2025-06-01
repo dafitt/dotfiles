3af0467d3f94054d2faccf6572ffe6ccf54116c6
dafitt <dafitt@posteo.me>
2025-06-01 17:18:39 +0200

    synchting: used hm integration

--- modules/home/syncthing/default.nix
+++ modules/home/syncthing/default.nix
@@ -12,7 +12,12 @@ in
 
   config = mkIf cfg.enable {
     # https://syncthing.net/
-    home.packages = with pkgs; [ syncthing ];
+    services.syncthing = {
+      enable = true;
+      guiAddress = "localhost:8384";
+      overrideDevices = false;
+      overrideFolders = false;
+    };
 
     xdg.desktopEntries.syncthing = {
       name = "Syncthing";
@@ -34,7 +39,6 @@ in
 
     wayland.windowManager.hyprland.settings = {
       bind = [ "SUPER_ALT, Z, exec, xdg-open https://localhost:8384" ];
-      exec-once = [ "uwsm app -- ${pkgs.syncthing}/bin/syncthing serve --no-browser" ];
     };
   };
 }
