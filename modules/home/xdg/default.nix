cb53c8981c63f30ef20abc14a2ee12a442f003d6
dafitt <dafitt@posteo.me>
2025-06-01 18:34:58 +0200

    xdg: added gtk bookmarks

--- modules/home/xdg/default.nix
+++ modules/home/xdg/default.nix
@@ -27,5 +27,16 @@ in
         };
       };
     };
+
+    gtk.gtk3.bookmarks = with config.xdg.userDirs; [
+      "file://${desktop}"
+      "file://${documents}"
+      "file://${download}"
+      "file://${music}"
+      "file://${pictures}"
+      "file://${publicShare}"
+      "file://${templates}"
+      "file://${videos}"
+    ];
   };
 }
