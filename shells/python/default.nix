2f19820add39a4d58e0fbb64355f9964380622b9
dafitt <dafitt@posteo.me>
2025-06-12 14:32:30 +0200

    shells: added python shell with python blame shutdown script

---
+++ shells/python/default.nix
@@ -0,0 +1,10 @@
+{ pkgs, inputs, ... }:
+
+#$ nix develop .#python
+pkgs.mkShell {
+  packages = with pkgs; with inputs; [
+    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+      tabulate
+    ]))
+  ];
+}
