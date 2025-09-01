2e1d7eba3688068d5ec30694d462c6a11c7b82a9
dafitt <dafitt@posteo.me>
2025-09-01 21:02:22 +0200

    yazi: configured opener

--- modules/home/editors/micro/default.nix
+++ modules/home/editors/micro/default.nix
@@ -16,72 +16,84 @@ in
     setAsDefaultEditor = mkEnableOption "making it the default EDITOR";
   };
 
-  config = mkIf cfg.enable {
-    # Modern and intuitive terminal-based text editor
-    # https://micro-editor.github.io/
-    programs.micro = {
-      enable = true;
-      settings = {
-        # https://github.com/zyedidia/micro/blob/master/runtime/help/options.md
-        colorscheme = "simple";
-        mkparents = true;
-        softwrap = true;
-        tabmovement = true;
-        tabsize = 4;
-        tabstospaces = true;
-        autosu = true;
-        eofnewline = false;
+  config = mkMerge [
+    (mkIf cfg.enable {
+      # Modern and intuitive terminal-based text editor
+      # https://micro-editor.github.io/
+      programs.micro = {
+        enable = true;
+        settings = {
+          # https://github.com/zyedidia/micro/blob/master/runtime/help/options.md
+          colorscheme = "simple";
+          mkparents = true;
+          softwrap = true;
+          tabmovement = true;
+          tabsize = 4;
+          tabstospaces = true;
+          autosu = true;
+          eofnewline = false;
+        };
       };
-    };
 
-    # https://github.com/zyedidia/micro/blob/master/assets/packaging/micro.desktop
-    xdg.desktopEntries.micro = {
-      name = "Micro";
-      icon = "micro.svg";
-      genericName = "Text Editor";
-      comment = "Edit text files in a terminal";
-      exec = "${pkgs.micro}/bin/micro %F";
-      terminal = true;
-      startupNotify = false;
-      type = "Application";
-      categories = [
-        "Utility"
-        "TextEditor"
-        "Development"
-      ];
-      mimeType = [
-        "text/plain"
-        "text/x-chdr"
-        "text/x-csrc"
-        "text/x-c++hdr"
-        "text/x-c++src"
-        "text/x-java"
-        "text/x-dsrc"
-        "text/x-pascal"
-        "text/x-perl"
-        "text/x-python"
-        "application/x-php"
-        "application/x-httpd-php3"
-        "application/x-httpd-php4"
-        "application/x-httpd-php5"
-        "application/xml"
-        "text/html"
-        "text/css"
-        "text/x-sql"
-        "text/x-diff"
-      ];
+      # https://github.com/zyedidia/micro/blob/master/assets/packaging/micro.desktop
+      xdg.desktopEntries.micro = {
+        name = "Micro";
+        icon = "micro.svg";
+        genericName = "Text Editor";
+        comment = "Edit text files in a terminal";
+        exec = "${getExe config.programs.micro.package} %F";
+        terminal = true;
+        startupNotify = false;
+        type = "Application";
+        categories = [
+          "Utility"
+          "TextEditor"
+          "Development"
+        ];
+        mimeType = [
+          "text/plain"
+          "text/x-chdr"
+          "text/x-csrc"
+          "text/x-c++hdr"
+          "text/x-c++src"
+          "text/x-java"
+          "text/x-dsrc"
+          "text/x-pascal"
+          "text/x-perl"
+          "text/x-python"
+          "application/x-php"
+          "application/x-httpd-php3"
+          "application/x-httpd-php4"
+          "application/x-httpd-php5"
+          "application/xml"
+          "text/html"
+          "text/css"
+          "text/x-sql"
+          "text/x-diff"
+        ];
 
-      settings = {
-        Keywords = "text;editor;syntax;terminal;";
+        settings = {
+          Keywords = "text;editor;syntax;terminal;";
+        };
       };
-    };
 
-    home.sessionVariables.EDITOR = mkIf cfg.setAsDefaultEditor (getExe config.programs.micro.package);
+      wayland.windowManager.hyprland.settings = {
+        bind = mkIf cfg.setAsDefaultEditor [
+          "SUPER_ALT, E, exec, uwsm app -- ${getExe pkgs.kitty} -e ${getExe config.programs.micro.package}"
+        ];
+      };
+    })
 
-    wayland.windowManager.hyprland.settings = {
-      bind = mkIf cfg.setAsDefaultEditor [
-        "SUPER_ALT, E, exec, uwsm app -- ${getExe pkgs.kitty} -e ${pkgs.micro}/bin/micro"
+    (mkIf cfg.setAsDefaultEditor {
+      home.sessionVariables.EDITOR = getExe config.programs.micro.package;
+
+      programs.yazi.settings.opener.edit = [
+        {
+          run = ''${getExe config.programs.micro.package} "$@"'';
+          block = true;
+          desc = "micro";
+        }
       ];
-    };
-  };
+    })
+  ];
 }
