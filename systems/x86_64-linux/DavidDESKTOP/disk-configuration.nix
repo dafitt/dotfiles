449644f782097625eea82a7b1afe2cd7cf3eac28
dafitt <dafitt@posteo.me>
2025-09-04 13:59:56 +0200

    DavidDESKTOP: moved impermanance initrd-script into a systemd unit

--- systems/x86_64-linux/DavidDESKTOP/disk-configuration.nix
+++ systems/x86_64-linux/DavidDESKTOP/disk-configuration.nix
@@ -69,31 +69,54 @@ with lib;
     };
   };
 
-  # boot.initrd.postResumeCommands = mkAfter ''
-  #   mkdir --parents /tmp/impermanance
-  #   mount ${config.disko.devices.disk.root.device}-part2 /tmp/impermanance
-  #   if [[ -e /tmp/impermanance/@home ]]; then
-  #       mkdir --parents /tmp/impermanance/@home.old.d
-  #       timestamp=$(date --date="@$(stat -c %Y /tmp/impermanance/@home)" "+%Y-%m-%-d_%H:%M:%S")
-  #       mv /tmp/impermanance/@home "/tmp/impermanance/@home.old.d/$timestamp"
-  #   fi
+  ## https://www.notashelf.dev/posts/impermanence/#impermanence
+  #boot.initrd.systemd = {
+  #  enable = true;
+  #  services.rollback = {
+  #    description = "Rollback BTRFS root drive to a pristine state";
+  #    wantedBy = [ "initrd.target" ];
 
-  #   delete_subvolume_recursively() {
-  #       IFS=$'\n'
-  #       for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-  #           delete_subvolume_recursively "/tmp/impermanance/$i"
-  #       done
-  #       btrfs subvolume delete "$1"
-  #   }
+  #    # LUKS/TPM process. If you have named your device mapper something other
+  #    # than 'enc', then @enc will have a different name. Adjust accordingly.
+  #    # after = [ "me @ domain" ];
 
-  #   for i in $(find /tmp/impermanance/@home.old.d/ -maxdepth 1 -mtime +30); do
-  #       delete_subvolume_recursively "$i"
-  #   done
+  #    before = [ "sysroot.mount" ];
 
-  #   btrfs subvolume create /tmp/impermanance/@home
-  #   umount /tmp/impermanance
-  #   rmdir --ignore-fail-on-non-empty --parents /tmp/impermanance
-  # '';
+  #    unitConfig.DefaultDependencies = "no";
+  #    serviceConfig.Type = "oneshot";
+  #    script = ''
+  #      echo "Mounting root BTRFS drive..."
+  #      mkdir --parents /mnt
+  #      mount --options "subvol=/" ${config.disko.devices.disk.root.device}-part2 /mnt
+
+  #      if [[ -e /mnt/@home ]]; then
+  #          mkdir --parents /mnt/home.old.d
+  #          timestamp=$(date --date="@$(stat --format=%Y /mnt/@home)" "+%Y-%m-%dT%H:%M:%S")
+  #          echo "Moving @home to 'home.old.d/$timestamp'..."
+  #          mv /mnt/@home "/mnt/home.old.d/$timestamp"
+  #      fi
+
+  #      delete_subvolume_recursively() {
+  #          IFS=$'\n'
+  #          for subvolume in $(btrfs subvolume list -o "$1" | cut --fields 9- --delimiter ' '); do
+  #              delete_subvolume_recursively "/mnt/$subvolume"
+  #          done
+  #          echo "Deleting subvolume '$subvolume'..."
+  #          btrfs subvolume delete "$1"
+  #      }
+  #      echo "Deleting very old subvolumes..."
+  #      for subvolume in $(find /mnt/home.old.d/ -maxdepth 1 -mtime +30); do
+  #          delete_subvolume_recursively "$subvolume"
+  #      done
+
+  #      echo "Creating new empty subvolume @home..."
+  #      btrfs subvolume create /mnt/@home
+
+  #      echo "Finished successfully. Unmounting..."
+  #      umount /mnt
+  #    '';
+  #  };
+  #};
 
   environment.persistence."/nix/persist" = {
     enable = true;
