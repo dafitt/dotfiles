3037dead15ce003dd6c0d7890bad94b66841ae90
dafitt <dafitt@posteo.me>
2025-12-01 17:06:37 +0100

    ly: Added uwsm fix

--- modules/nixos/displayManager/ly/default.nix
+++ modules/nixos/displayManager/ly/default.nix
@@ -19,5 +19,8 @@ in
     services.displayManager.ly = {
       enable = true;
     };
+
+    # https://codeberg.org/fairyglade/ly/issues/727
+    systemd.services.display-manager.environment.XDG_CURRENT_DESKTOP = "X-NIXOS-SYSTEMD-AWARE";
   };
 }
