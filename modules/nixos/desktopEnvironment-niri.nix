b522cece1763278718ff56dd67cfbd11ce7b9567
dafitt <dafitt@posteo.me>
2025-12-28 18:31:31 +0100

    modules/niri: Added uwsm session

--- modules/nixos/desktopEnvironment-niri.nix
+++ modules/nixos/desktopEnvironment-niri.nix
@@ -1,4 +1,12 @@
 {
   programs.niri.enable = true;
   programs.uwsm.enable = true;
+
+  programs.uwsm.waylandCompositors = {
+    niri = {
+      prettyName = "Niri";
+      comment = "Niri compositor managed by UWSM";
+      binPath = "/run/current-system/sw/bin/niri --session";
+    };
+  };
 }
