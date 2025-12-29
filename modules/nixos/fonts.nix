3a9307cda94c78bfc916d5cf67fadbb1253f3f8c
dafitt <dafitt@posteo.me>
2025-12-29 11:46:45 +0100

    modules/fonts: enableDefaultPackages

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -5,13 +5,16 @@
     LOG_ICONS = "true";
   };
 
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.noto
-    nerd-fonts.symbols-only
-    nerd-fonts.ubuntu
-    nerd-fonts.ubuntu-mono
-    nerd-fonts.ubuntu-sans
-    open-sans
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.noto
+      nerd-fonts.symbols-only
+      nerd-fonts.ubuntu
+      nerd-fonts.ubuntu-mono
+      nerd-fonts.ubuntu-sans
+      open-sans
+    ];
+  };
 }
