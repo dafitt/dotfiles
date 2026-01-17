a4957846775b74049bfb758b501bdcecb2793fac
dafitt <dafitt@posteo.me>
2026-01-17 16:26:59 +0100

    modules/fish: Fixed complete commands

--- hosts/DavidISO/users/david.nix
+++ hosts/DavidISO/users/david.nix
@@ -32,31 +32,31 @@ with lib;
       ''
         complete --command cp --arguments "/iso/dotfiles ."
 
-        complete --command chmod --long recursive --short R --no-files
+        complete --command chmod --long-option recursive --short-option R --no-files
         complete --command chmod --arguments "u+w dotfiles/"
 
-        complete --command nixos-generate-config --long help --no-files
-        complete --command nixos-generate-config --long no-filesystems --no-files
-        complete --command nixos-generate-config --long root --require-parameter --arguments "/mnt"
+        complete --command nixos-generate-config --long-option help --no-files
+        complete --command nixos-generate-config --long-option no-filesystems --no-files
+        complete --command nixos-generate-config --long-option root --require-parameter --arguments "/mnt"
 
-        complete --command disko --long help --no-files
-        complete --command disko --long dry-run --no-files
-        complete --command disko --long yes-wipe-all-disks --no-files
-        complete --command disko --long root-mountpoint --require-parameter --arguments "/mnt"
-        complete --command disko --long mode --short m --require-parameter --no-files --arguments "destroy format mount unmount format,mount destroy,format,mount"
-        complete --command disko --long flake --require-parameter --arguments "${flakePathArguments}"
+        complete --command disko --long-option help --no-files
+        complete --command disko --long-option dry-run --no-files
+        complete --command disko --long-option yes-wipe-all-disks --no-files
+        complete --command disko --long-option root-mountpoint --require-parameter --arguments "/mnt"
+        complete --command disko --long-option mode --short-option m --require-parameter --no-files --arguments "destroy format mount unmount format,mount destroy,format,mount"
+        complete --command disko --long-option flake --require-parameter --arguments "${flakePathArguments}"
 
         complete --command mkdir --arguments "/mnt/persist{/var/log/journal,/var/lib}"
         complete --command mount --arguments "{/mnt/persist,/mnt}/var/log/journal {/mnt/persist,/mnt}/var/lib"
 
-        complete --command nixos-install --long help --no-files
-        complete --command nixos-install --long no-root-passwd --no-files
-        complete --command nixos-install --long flake --require-parameter --arguments "${flakePathArguments}"
+        complete --command nixos-install --long-option help --no-files
+        complete --command nixos-install --long-option no-root-passwd --no-files
+        complete --command nixos-install --long-option flake --require-parameter --arguments "${flakePathArguments}"
 
-        complete --command disko-install --long help --no-files
-        complete --command disko-install --long disk --require-parameter --no-files --arguments "main"
-        complete --command disko-install --long write-efi-boot-entries --no-files
-        complete --command disko-install --long flake --require-parameter --arguments "${flakePathArguments}"
+        complete --command disko-install --long-option help --no-files
+        complete --command disko-install --long-option disk --require-parameter --no-files --arguments "main"
+        complete --command disko-install --long-option write-efi-boot-entries --no-files
+        complete --command disko-install --long-option flake --require-parameter --arguments "${flakePathArguments}"
       '';
   };
 }
