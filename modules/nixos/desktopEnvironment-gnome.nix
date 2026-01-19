cb2f4505129d7273fef2f7e3ede468a0bf9325d6
dafitt <dafitt@posteo.me>
2026-01-19 15:44:39 +0100

    modules/gnome: Disabled gnome app suite

--- modules/nixos/desktopEnvironment-gnome.nix
+++ modules/nixos/desktopEnvironment-gnome.nix
@@ -2,4 +2,12 @@
 {
   services.desktopManager.gnome.enable = true;
   services.udev.packages = [ pkgs.gnome-settings-daemon ];
+
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    gnome-user-docs
+  ];
 }
