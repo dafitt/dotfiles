b419f8a60e1019a4f93020a712cdccf0c2a854fb
dafitt <dafitt@posteo.me>
2026-01-21 14:35:43 +0100

    hosts/TT-WCN00692: Changed nix repl comment

--- hosts/TT-WCN00692/users/sdc2tt.nix
+++ hosts/TT-WCN00692/users/sdc2tt.nix
@@ -7,7 +7,7 @@
   ...
 }:
 with lib;
-#> perSystem.self.homeConfigurations."sdc2tt@TT-WCN00692"
+#> legacyPackages.x86_64-linux.homeConfigurations."sdc2tt@TT-WCN00692".config
 #$ home-manager --flake .#sdc2tt@TT-WCN00692 switch
 {
   home.stateVersion = "25.11";
