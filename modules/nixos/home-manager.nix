c89c54a441d0d4aa0c5316be3388d9fbcc1d1f22
dafitt <dafitt@posteo.me>
2026-01-21 14:56:25 +0100

    modules/home-manager: Added recommended configuration

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -5,4 +5,10 @@
   home-manager = {
     backupFileExtension = "hm-old"; # Move existing files rather than failing
   };
+
+  # https://github.com/nix-community/home-manager/blob/27b60942b7285824937f8c2c05021370f6fc5904/modules/misc/xdg-portal.nix#L39C9-L39C89
+  environment.pathsToLink = [
+    "/share/xdg-desktop-portal"
+    "/share/applications"
+  ];
 }
